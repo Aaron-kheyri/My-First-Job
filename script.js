d4ddfa8f939cee8a12547043bff2849dcb745a26
Aaron-kheyri <Aaron.Kheyri@pgs.vic.edu.au>
2026-07-29 18:22:58 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const startButton = document.getElementById("startButton");
 
 const homeScreen = document.getElementById("homeScreen");
-
 const characterScreen = document.getElementById("characterScreen");
-
 const jobScreen = document.getElementById("jobScreen");
+const shiftScreen = document.getElementById("shiftScreen");
+
+
+let playerName = "";
+let playerJob = "";
 
 
 startButton.onclick = function () {
 
     homeScreen.style.display = "none";
-
     characterScreen.style.display = "block";
 
 };
@@ -18,22 +20,64 @@ startButton.onclick = function () {
 
 document.getElementById("continueButton").onclick = function () {
 
-    const name = document.getElementById("playerName").value;
+    playerName = document.getElementById("playerName").value;
 
-    if (name === "") {
+
+    if (playerName === "") {
 
         alert("Please enter your name!");
-
         return;
 
     }
 
 
     characterScreen.style.display = "none";
-
     jobScreen.style.display = "block";
 
+
     document.getElementById("welcomeText").innerHTML =
-    "Welcome " + name + "! Choose your first job.";
+    "Welcome " + playerName + "! Choose your first job.";
 
 };
+
+
+
+const jobButtons = document.querySelectorAll(".jobButton");
+
+
+jobButtons[0].onclick = function() {
+
+    chooseJob("🍔 Fast Food Worker");
+
+};
+
+
+jobButtons[1].onclick = function() {
+
+    chooseJob("🛒 Retail Assistant");
+
+};
+
+
+jobButtons[2].onclick = function() {
+
+    chooseJob("☕ Café Worker");
+
+};
+
+
+
+function chooseJob(job) {
+
+    playerJob = job;
+
+
+    jobScreen.style.display = "none";
+
+    shiftScreen.style.display = "block";
+
+
+    document.getElementById("jobWelcome").innerHTML =
+    playerName + ", you are now a " + playerJob + "!";
+
+}
