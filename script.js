7a96a5fc3631d5d10aacb1a4b95f07143554a25a
Aaron-kheyri <Aaron.Kheyri@pgs.vic.edu.au>
2026-07-29 17:14:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-document.getElementById("startButton").onclick = function() {
-    alert("Welcome to My First Job!");
+const startButton = document.getElementById("startButton");
+
+const homeScreen = document.getElementById("homeScreen");
+
+const characterScreen = document.getElementById("characterScreen");
+
+startButton.onclick = function () {
+
+    homeScreen.style.display = "none";
+
+    characterScreen.style.display = "block";
+
+};
+
+document.getElementById("continueButton").onclick = function () {
+
+    const name = document.getElementById("playerName").value;
+
+    if (name === "") {
+
+        alert("Please enter your name!");
+
+        return;
+
+    }
+
+    alert("Welcome " + name + "! Your journey begins.");
+
 };
